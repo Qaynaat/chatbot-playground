1fd90c2cdf2786c7aeaf2789ddfc1edad506dd73
Kainat Aslam <kaynaat617@gmail.com>
2026-08-27 16:29:21 +0500

    Complete Mission 009 with Python modules

---
+++ calculator.py
@@ -0,0 +1,8 @@
+from math_tools import add,subtract,multiply,divide
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Addition: ", add(num1,num2))
+print("Difference: ", subtract(num1,num2))
+print("Product: ", multiply(num1,num2))
+print("Division: ", divide(num1,num2))
