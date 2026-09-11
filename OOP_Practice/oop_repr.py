752dab3dd6d15512b3712e26c9dcbff8b431c7c4
Kainat Aslam <kaynaat617@gmail.com>
2026-09-11 20:33:05 +0500

    Complete Mission 017 with repr

---
+++ OOP_Practice/oop_repr.py
@@ -0,0 +1,17 @@
+class Student():
+    def __init__(self , name , age ):
+        self.name = name 
+        self.age = age
+
+    def __str__(self):
+        return f"\nName: {self.name} \nAge: {self.age}\n"
+
+    def __repr__(self):
+        return f"Student(name='{self.name}' , age='{self.age}')\n"
+
+s1 = Student("Aou" , 27)
+print(s1)
+
+print(repr(s1))
+print(str(s1))
+print([s1])
